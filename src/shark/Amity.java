b02b2919d9e8b8a3c77f6abc9c4b018c5734ed15
keiranjames <keiran.marrett-hitch@brentwood.ca>
2015-11-13 09:05:29 -0800

    Key Press/Release
    Friday the 13th

--- src/shark/Amity.java
+++ src/shark/Amity.java
@@ -6,7 +6,10 @@
 package shark;
 
 import environment.Environment;
+import grid.Grid;
+import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Point;
 import java.awt.event.KeyEvent;
 import java.awt.event.MouseEvent;
 
@@ -16,32 +19,82 @@ import java.awt.event.MouseEvent;
  */
 class Amity extends Environment {
 
+    Grid grid;
+    
     public Amity() {
+        
+        grid = new Grid(25, 20, 20, 20, new Point (180, 80), Color.red);
         
     }
 
     @Override
     public void initializeEnvironment() {
     }
+    
+    int counter;
 
     @Override
     public void timerTaskHandler() {
+//        System.out.println("Aidan is cool! " + ++counter );
     }
 
     @Override
     public void keyPressedHandler(KeyEvent e) {
+//        System.out.println("Key Event " + e.getKeyChar()); 
+//        System.out.println("Key Event " + e.getKeyCode()); 
+        if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+            System.out.println("Go Left");
+        }
+        else if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+            System.out.println("Go Right");
+        }
+        else if (e.getKeyCode() == KeyEvent.VK_UP) {
+            System.out.println("Go Up");
+        }
+        
+        else if (e.getKeyCode() == KeyEvent.VK_DOWN) {
+            System.out.println("Go Down");
+        }
+        
     }
-
+        
+        
+        
+        
+        
+        
     @Override
     public void keyReleasedHandler(KeyEvent e) {
+        
+        if (e.getKeyCode() == KeyEvent.VK_W) {
+            System.out.println("Go Up");
+        }
+        else if (e.getKeyCode() == KeyEvent.VK_A) {
+            System.out.println("Go Left");
+        }
+        else if (e.getKeyCode() == KeyEvent.VK_S) {
+            System.out.println("Go Down");
+        }
+        
+        else if (e.getKeyCode() == KeyEvent.VK_D) {
+            System.out.println("Go Right");
+        }
+        
+        
     }
 
     @Override
     public void environmentMouseClicked(MouseEvent e) {
+        System.out.println("Mouse clicked at " + e.getPoint());
+        System.out.println("You are in cell " + grid.getCellLocationFromSystemCoordinate(e.getPoint()));
+        
     }
 
     @Override
     public void paintEnvironment(Graphics graphics) {
+        if (grid != null) {
+            grid.paintComponent(graphics);
+        }
     }
     
 }
